47595ad690225cd87b0ba8c735abd52c6c432de6
Gabriel Castro <e.g.castro.dl@gmail.com>
2018-06-20 14:43:05 -0300

    part 3

--- src/aura/CompensationsTableRow/CompensationsTableRowHelper.js
+++ src/aura/CompensationsTableRow/CompensationsTableRowHelper.js
@@ -13,5 +13,41 @@
         "slideDevName": 'related'
     })
     sObjectEvent.fire();
-		}
+		},
+		changeStatus : function(component){
+			var action = component.get('c.updateStatus');
+			action.setParams({ strFilters : JSON.stringify(component.get('v.component'))});
+			console.log(JSON.stringify(component.get('v.component')));
+
+			// Set up the callback
+			var self = this;
+			action.setCallback(this, function(response) {
+			var state = response.getState();
+			if (state === "SUCCESS") {
+					// Alert the user with the value returned 
+					// from the server
+					//component.set('v.components', response.getReturnValue());
+					console.log("Done!");
+					// You would typically fire a event here to trigger 
+					// client-side notification that the server-side 
+					// action is complete
+			}
+			else if (state === "INCOMPLETE") {
+					// do something
+			}
+			else if (state === "ERROR") {
+					var errors = response.getError();
+					if (errors) {
+							if (errors[0] && errors[0].message) {
+									console.log("Error message: " + 
+													errors[0].message);
+							}
+					} else {
+							console.log("Unknown error");
+					}
+			}
+			});
+
+			$A.enqueueAction(action);
+			}
 })
